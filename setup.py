3570c204a4052145d96e696ec00ac7333759a799
wagoodman <wagoodman@gmail.com>
2017-08-04 08:54:51 -0400

    bump version for new relic

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from glob import glob
 
 setup(
     name='bridgy',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/wagoodman/bridgy',
     license='MIT',
     author='Alex Goodman',
